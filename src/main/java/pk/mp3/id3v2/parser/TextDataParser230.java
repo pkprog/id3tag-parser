56472051a6378cc530d126f1bd8818d610790b31
Pavel Kh <pk.prog@yandex.ru>
2015-12-28 16:29:35 +0300

    After remake TextParser

--- src/main/java/pk/mp3/id3v2/parser/TextDataParser230.java
+++ src/main/java/pk/mp3/id3v2/parser/TextDataParser230.java
@@ -14,12 +14,53 @@ public class TextDataParser230 implements TextDataParser {
     private static final Charset UNIDODE_BIG_ENDIAN = StandardCharsets.UTF_16BE;
     private static final Charset UNIDODE_LITTLE_ENDIAN = StandardCharsets.UTF_16LE;
 
-//    private static final int DEFAULT_TERMINATED_STRING = 0x00;
-//    private static final int UNICODE_TERMINATED_STRING = 0x0000;
-
     private static final int DEFAULT_NEW_LINE = 0x0a;
 
     public Charset getCharset(byte[] data) {
+        if (data == null) return DEFAULT_CHARSET;
+
+        if (data.length == 1) return DEFAULT_CHARSET;
+
+        if (data.length == 2) {
+            byte byte1 = data[0], byte2 = data[1];
+
+            if ((byte1 & 0xff) == 0xfe && (byte2 & 0xff) == 0xff) { //UNICODE big endian
+                return UNIDODE_BIG_ENDIAN;
+            }
+            if ((byte1 & 0xff) == 0xff && (byte2 & 0xff) == 0xfe) { //UNICODE little endian
+                return UNIDODE_LITTLE_ENDIAN;
+            }
+
+            return DEFAULT_CHARSET;
+        } else {
+            byte byte1 = data[0], byte2 = data[1], byte3 = data[2];
+
+            if (getDifferentEncodingFlag(data).equals(DifferentEncodingFlag.ISO_8859_1)) {
+                return DEFAULT_CHARSET;
+            }
+
+            if (getDifferentEncodingFlag(data).equals(DifferentEncodingFlag.NONE)) {
+                if ((byte1 & 0xff) == 0xfe && (byte2 & 0xff) == 0xff) { //UNICODE big endian
+                    return UNIDODE_BIG_ENDIAN;
+                }
+                if ((byte1 & 0xff) == 0xff && (byte2 & 0xff) == 0xfe) { //UNICODE little endian
+                    return UNIDODE_LITTLE_ENDIAN;
+                }
+            }
+
+            if (getDifferentEncodingFlag(data).equals(DifferentEncodingFlag.UNICODE)) {
+                if ((byte2 & 0xff) == 0xfe && (byte3 & 0xff) == 0xff) { //UNICODE big endian
+                    return UNIDODE_BIG_ENDIAN;
+                }
+
+                if ((byte2 & 0xff) == 0xff && (byte3 & 0xff) == 0xfe) { //UNICODE little endian
+                    return UNIDODE_LITTLE_ENDIAN;
+                }
+            }
+
+            return DEFAULT_CHARSET;
+        }
+/*
         if (data != null && data.length >= 3) {
             byte byte1 = data[0], byte2 = data[1], byte3 = data[2];
 
@@ -49,11 +90,13 @@ public class TextDataParser230 implements TextDataParser {
                 }
             }
         }
-
-        return DEFAULT_CHARSET;
+*/
+//        return DEFAULT_CHARSET;
     }
 
     public DifferentEncodingFlag getDifferentEncodingFlag(byte[] data) {
+        if (data == null || data.length == 0) return DifferentEncodingFlag.NONE;
+
         byte byte1 = data[0];
         if ((byte1 & 0xff) == 0) { //Frames that allow different types of text encoding for ISO-8859-1
             return DifferentEncodingFlag.ISO_8859_1;
@@ -65,6 +108,18 @@ public class TextDataParser230 implements TextDataParser {
     }
 
     public TerminatingString getTerminatingString(byte[] data) {
+        Charset charset = getCharset(data);
+
+        if (charset.equals(UNIDODE_BIG_ENDIAN)) {
+            return TerminatingString.UNICODE;
+        }
+        if (charset.equals(UNIDODE_LITTLE_ENDIAN)) {
+            return TerminatingString.UNICODE;
+        }
+        if (charset.equals(DEFAULT_CHARSET)) {
+            return TerminatingString.DEFAULT;
+        }
+/*
         if (data != null && data.length >= 2) {
             byte byte1 = data[0], byte2 = data[1];
 
@@ -80,6 +135,7 @@ public class TextDataParser230 implements TextDataParser {
                 return TerminatingString.DEFAULT;
             }
         }
+*/
 
         return TerminatingString.DEFAULT;
     }
@@ -89,11 +145,6 @@ public class TextDataParser230 implements TextDataParser {
 
         if (getDifferentEncodingFlag(data).equals(DifferentEncodingFlag.NONE)) {
             if (getCharset(data).equals(DEFAULT_CHARSET)) {
-//                if ((data[0] & 0xff) == 0x00) {
-//                    return Arrays.copyOfRange(data, 1, data.length);
-//                } else {
-//                    return data;
-//                }
                 return data;
             } else if (getCharset(data).equals(UNIDODE_BIG_ENDIAN)) {
                 return Arrays.copyOfRange(data, 2, data.length);
